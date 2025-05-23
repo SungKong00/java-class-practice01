abc9c4ec8f6a5c1954759de8e342052cdea2dc73
NohSungBeen <castlekong1019@gmail.com>
2025-05-23 23:03:52 +0900

    feat: 결제수단 전략패턴 적용 구현
    - 전략패턴을 적용하여 결제수단 구현

---
+++ shoppingPractice/src/main/java/org/univ/startegy/CreditCardPayment.java
@@ -0,0 +1,18 @@
+package org.univ.startegy;
+
+public class CreditCardPayment implements PaymentMethod {
+    private String cardNumber;
+    private String expiryDate;
+    // CVV 필드는 구현하지 않음
+
+    public CreditCardPayment(String cardNumber, String expiryDate) {
+        // TODO: 카드번호, 만료일 유효성 검사
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public void pay(double amount) {
+        // TODO: 카드 결제 로직, 실패 시 PaymentException 발생
+    }
+}
